57992e4b43829b280ee7548abb2b6090b72d6ebe
ho.hosseini <Ho.Hosseini@sadad.co.ir>
2023-03-07 16:45:15 +0330

    make first view

--- ecommerce_drf/product/views.py
+++ ecommerce_drf/product/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import response, viewsets
+
+from .models import Category
+from .serializers import CategorySerializer
 
 # Create your views here.
+
+
+def home(req):
+    return render(req, "index.html")
+
+
+class CategoryViewSet(viewsets.ViewSet):
+    """
+    A simple Viewset for viewing all categories
+    """
+
+    queryset = Category.objects.all()
+
+    def list(self, req):
+        serializers = CategorySerializer(self.queryset, many=True)
+        print(serializers.data)
+        print(serializers)
+        return response(serializers.data)
--- ecommerce_drf/urls.py
+++ ecommerce_drf/urls.py
@@ -2,7 +2,7 @@
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
+Examples:W
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from ecommerce_drf.product import views
+
+from .views import home
+
+router = DefaultRouter()
+router.register(r"category", views.CategoryViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", home),
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
 ]
